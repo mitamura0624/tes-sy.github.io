ecbf0f3d684d18a7170a32de8014b1785fde3a2c
U-DENA\yusei.shimogawa <yusei.shimogawa@O-05819-WIN.dena.local>
2015-09-03 17:48:29 +0900

    tutorial study started

--- hajipa-tutorial-study/hajipa-tutorial-1.pl
+++ hajipa-tutorial-study/hajipa-tutorial-1.pl
@@ -1,3 +1,48 @@
-# test proglams
+#!/usr/bin/perl
+# your code goes here
 
-print "hello world!^-^";
+# use strict;
+
+$define_pi = (2 * 3.14159264);
+# hajipa pages is 51
+# question1
+$circumference = 12.5 * $define_pi;
+print $circumference."\n";
+
+# question2
+print "input radius\n";
+$input_radius = <STDIN>;
+$circumference2 = $input_radius * $define_pi;
+
+print $circumference2."\n";
+
+# question3
+print "input radius2\n";
+$input_radius2 = <STDIN>;
+if($input_radius2 >= 0){
+	$circumference3 = $input_radius2 * $define_pi;
+	print $circumference3."\n";
+} else {
+	print "0\n";
+}
+
+# question4
+print "input first number:";
+$input_num1 = <STDIN>;
+print "\n";
+print "input second number:";
+$input_num2 = <STDIN>;
+print "\n";
+
+$ans = $input_num1 * $input_num2;
+print "Ans:".$ans."\n";
+
+# question5
+
+print "input strings\n";
+$input_strings = <STDIN>;
+
+print "input number\n";
+$input_num = <STDIN>;
+
+print $input_strings x $input_num;
